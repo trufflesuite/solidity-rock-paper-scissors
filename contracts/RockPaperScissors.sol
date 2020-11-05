f7863cdda58eec88e6d139932e97115356f023ba
Mike Seese <seesemichaelj@gmail.com>
2020-11-05 00:40:49 -0800

    wut?

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -18,7 +18,7 @@ contract RockPaperScissors {
     _token = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
   }
 
-  function approve() public {
+  function approve(uint8 makeMeZero) public {
     _token.approve(address(this), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
   }
 
